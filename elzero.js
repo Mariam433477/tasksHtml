32482fe98884b7ff1763c52fbc2f5128a077dc33
Mariam433477 <https://github.com/Mariam433477>
2022-03-06 02:42:45 +0200

    create project

---
+++ elzero.js
@@ -0,0 +1,52 @@
+/*
+var num1=prompt("Enter your num1");
+var num2=prompt("Enter your num2");
+if(parseInt(num1)>parseInt(num2)){
+    document.write(num1);
+}
+else if(parseInt(num2)>parseInt(num1)){
+    document.write(num2);
+}
+else{
+    document.write("equal");
+} */
+
+/*
+var num1=prompt("Enter your num1");
+var num2=prompt("Enter your num2");
+if(parseInt(num1)==parseInt(num2)){
+    document.write(3*(parseInt(num1)+parseInt(num2)));
+}
+else{
+  document.write(parseInt(num1)+parseInt(num2));
+}
+*/
+/*
+var num=prompt("Enter your num");
+var name=prompt("Enter your name");
+for(var i=0;i<num;i++){
+    document.write("<h1>Hello "+name+"</h1>");
+}
+*/
+
+var num=prompt("Enter your num");
+if(num%2!=0){
+    alert("num is odd");
+}
+else{
+    alert("num is even");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
